35f13167c8db6eb544aa0f302c58e6805e09ca7a
yanar <yanartas2007@gmail.com>
2025-11-07 10:10:27 +0300

    добавить домашку по ООП и pdf заданий

---
+++ domashka7/BankAccount.java
@@ -0,0 +1,47 @@
+package domashka7;
+
+public class BankAccount {
+    private String accountNumber;
+    private String ownerName;
+    private double balance;
+    private String currency;
+    private boolean isActive;
+
+    private BankAccount(String accountNumber, String ownerName, double balance, String currency, boolean isActive) {
+        this.accountNumber = accountNumber;
+        this.ownerName = ownerName;
+        this.balance = balance;
+        this.currency = currency;
+        this.isActive = isActive;
+    }
+
+    private BankAccount(String accountNumber, String ownerName) {
+        this(accountNumber, ownerName, 0.0, "валютакакаято", false);
+    }
+
+    private BankAccount() {
+        this("-1", "Имя");
+    }
+
+    private String displayInfo() {
+        return accountNumber + '_' + currency + '_' + balance;
+    }
+
+    private void deposit(double d) {
+        balance += d;
+    }
+
+    private void withdraw(double w) {
+        assert balance >= w;
+        balance -= w;
+    }
+
+    private void activateAccount() {
+        isActive = true;
+    }
+
+    private  void deactivateAccount() {
+        isActive = false;
+    }
+
+}
---
+++ domashka7/MobilePhone.java
@@ -0,0 +1,46 @@
+package domashka7;
+
+public class MobilePhone {
+
+    private String brand;
+    private String model;
+    private int storageGB;
+    private int batteryLevel;
+    private boolean isPoweredOn;
+
+    public MobilePhone(String brand, String model, int storageGB, int batteryLevel, boolean isPoweredOn) {
+        this.brand = brand;
+        this.model = model;
+        this.storageGB = storageGB;
+        this.batteryLevel = batteryLevel;
+        this.isPoweredOn = isPoweredOn;
+    }
+
+    public MobilePhone(String brand, String model, int storageGB) {
+        this(brand, model, storageGB, 0, false);
+    }
+
+    public MobilePhone() {
+        this("какаятомарка", "какаятомодель", 0);
+    }
+
+    public String displayInfo() {
+        return brand + '_' + model;
+    }
+
+    public void powerOn() {
+        isPoweredOn = true;
+    }
+
+    public void powerOff() {
+        isPoweredOn = false;
+    }
+
+    public void chargeBattery() {
+        batteryLevel = 100;
+    }
+
+    public void useBattery() {
+        batteryLevel = 0;
+    }
+}
