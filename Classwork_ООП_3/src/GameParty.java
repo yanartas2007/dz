7b4cf38651cec0317f35fe3118d0960178a52de2
yanar <yanartas2007@gmail.com>
2025-11-18 17:20:01 +0300

    Добавил классную ооп3

---
+++ Classwork_ООП_3/src/GameParty.java
@@ -0,0 +1,55 @@
+public class GameParty {
+    public static GameCharacter[] partyMembers = new GameCharacter[100]; // не хочу делать систему с массивом и его расширением каждый раз, так как уже делал подобное в studentsООП. Сделаю другую систему, максимальное количество будет ограничено
+    public static int characters_int = 0;
+
+    public static void addCharacter(GameCharacter character) {
+        assert characters_int < 100;
+        characters_int += 1;
+        partyMembers[characters_int] = character;
+    }
+
+    public static void removeCharacter(String name) {
+        int i = 0;
+        boolean flag = false;
+        for (GameCharacter g: partyMembers) {
+            if (g.getName().equals(name)) {flag = true;}
+            i++;
+            partyMembers[i-1] = partyMembers[i];
+        }
+    }
+
+    public static void displayPartyInfo() {
+        for(GameCharacter g: partyMembers) {
+            if (g != null) {
+            g.displayInfo();}
+        }
+    }
+
+    public static void useAllSpecialAbilities() {
+        for(GameCharacter g: partyMembers) {
+            if (g != null) {
+                g.useSpecialAbility();}
+        }
+    }
+
+    public static GameCharacter[] findCharacterByClass(String className) {
+        GameCharacter[] ret = new GameCharacter[100];
+        int i =0;
+        for(GameCharacter g: partyMembers) {
+            if (g.getClass().toString().equals(className)) {
+                ret[i] = g;}
+            i++;
+        }
+        return ret;
+    }
+
+    public static double calculatePartyPower() {
+        double power = 0;
+        for(GameCharacter g: partyMembers) {
+            if (g != null) {
+                power += g.getMana();}
+        }
+        return power;
+    }
+
+}
---
+++ Classwork_ООП_3/src/Warrior.java
@@ -0,0 +1,23 @@
+public class Warrior extends GameCharacter{
+    private double strength;
+    private double armor;
+    private String weaponType;
+
+    public Warrior() {
+        super();
+        strength = 0;
+        armor = 0;
+        weaponType = "";
+    }
+
+    public String displayInfo() {
+        return weaponType;
+    }
+
+    public void useSpecialAbility() {
+        System.out.println("БЕРСЕРК");
+    }
+
+    public void attack() {System.out.println(getName() + " Атакует");}
+    public void attack(String weapon) {System.out.println(getName() + " Атакует используя" + weapon);}
+}
